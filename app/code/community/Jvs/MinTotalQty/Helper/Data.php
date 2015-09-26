b161f085d976c86cfe529064103269e83af565ce
Javier Villanueva <javiervd@gmail.com>
2015-09-26 21:27:00 +0100

    Initial commit

---
+++ app/code/community/Jvs/MinTotalQty/Helper/Data.php
@@ -0,0 +1,72 @@
+<?php
+/**
+ * MinTotalQty data helper
+ *
+ * @category Jvs
+ * @package  Jvs_MinTotalQty
+ * @author   Javier Villanueva <javiervd@gmail.com>
+ */
+class Jvs_MinTotalQty_Helper_Data extends Mage_CatalogInventory_Helper_Minsaleqty
+{
+    const XML_PATH_MIN_TOTAL_QTY = 'cataloginventory/item_options/min_total_qty';
+
+    /**
+     * Retrieve min_total_qty value from config
+     *
+     * @param  int   $customerGroupId
+     * @param  mixed $store
+     * @return float|null
+     */
+    public function getConfigValue($customerGroupId, $store = null)
+    {
+        $value = Mage::getStoreConfig(self::XML_PATH_MIN_TOTAL_QTY, $store);
+        $value = $this->_unserializeValue($value);
+        if ($this->_isEncodedArrayFieldValue($value)) {
+            $value = $this->_decodeArrayFieldValue($value);
+        }
+        $result = null;
+        foreach ($value as $groupId => $qty) {
+            if ($groupId == $customerGroupId) {
+                $result = $qty;
+                break;
+            } else if ($groupId == Mage_Customer_Model_Group::CUST_GROUP_ALL) {
+                $result = $qty;
+            }
+        }
+        return $this->_fixQty($result);
+    }
+
+    /**
+     * Check if quote meets the minimun quantity
+     * of total items for a specific customer
+     *
+     * @todo   Change to more meaningful name
+     *
+     * @param  Mage_Sales_Model_Quote       $quote
+     * @param  Mage_Customer_Model_Customer $customer
+     * @return int|false
+     */
+    public function minimunOrderQty(Mage_Sales_Model_Quote $quote, Mage_Customer_Model_Customer $customer)
+    {
+        $minQtyForCustomer = Mage::helper('jvs_mintotalqty')
+                                ->getConfigValue($customer->getGroupId());
+
+        if ($quote->getItemsQty() < $minQtyForCustomer && $quote->getItemsQty() !== 0) {
+            return $minQtyForCustomer;
+        }
+
+        return false;
+    }
+
+    /**
+     * Check if current page is shopping cart
+     *
+     * @return boolean
+     */
+    public function isCartPage()
+    {
+        $frontController = Mage::app()->getFrontController();
+
+        return ($frontController->getAction()->getFullActionName() === 'checkout_cart_index');
+    }
+}
---
+++ app/code/community/Jvs/MinTotalQty/Model/Observer.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * MinTotalQty observer model
+ *
+ * @category Jvs
+ * @package  Jvs_MinTotalQty
+ * @author   Javier Villanueva <javiervd@gmail.com>
+ */
+class Jvs_MinTotalQty_Model_Observer extends Mage_CatalogInventory_Helper_Minsaleqty
+{
+    public function checkTotalQtyBeforeCheckout(Varien_Event_Observer $observer)
+    {
+        $quote    = $observer->getDataObject();
+        $customer = Mage::helper('customer')->getCustomer();
+
+        // If the minimun total quantity is not met
+        // redirect to cart page with error message
+        if ($minQty = Mage::helper('jvs_mintotalqty')->minimunOrderQty($quote, $customer)) {
+            Mage::getSingleton('checkout/session')->addUniqueMessages(
+                Mage::getSingleton('core/message')
+                    ->error(
+                        Mage::helper('cataloginventory')
+                            ->__(
+                                'The minimum quantity allowed for purchase is %s.',
+                                $minQty
+                            )
+                    )
+            );
+
+            // Check if we are not already on the cart page
+            if (!Mage::helper('jvs_mintotalqty')->isCartPage()) {
+                Mage::app()->getFrontController()->getResponse()
+                    ->setRedirect(Mage::getUrl('checkout/cart'));
+
+                Mage::app()->getRequest()->setDispatched(true);
+            }
+        }
+    }
+}
